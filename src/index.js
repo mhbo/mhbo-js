75ba669f37705f456612dda316a2b37b6db39747
Jim Jeffers <jim@sumocreations.com>
2018-05-14 09:32:47 -0700

    Added docs to main client interface.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ const homes = require("./homes")
 
 import type { Environment, MHBOApiClient, Credentials } from "./types.flow.js"
 
-const Client = (
+/**
+ * Initializes a new instance of the MHBO API Client.
+ *
+ * @param {string} apiAccessKey The access key used to identify the account connecting to the API.
+ * @param {string} apiSecret The secret key used to sign requests for this account.
+ * @param {?Environment} environment An optional override if a developer needs to test the client against a local development environment.
+ * @returns {MHBOApiClient} A wrapper for interacting with the MHBO API.
+ */
+function Client(
   apiAccessKey: string,
   apiSecret: string,
   environment: ?Environment
-): MHBOApiClient => {
+): MHBOApiClient {
   const creds: Credentials = { apiAccessKey, apiSecret }
-  return { homes: homes(environment, creds) }
+  return { homes: homes(creds, environment) }
 }
 
-module.exports = {
-  Client
-}
+module.exports = Client
